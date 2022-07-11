b9e9ff482c695cd2d3d0bbf4986c8d7c56ace2c3
Yaping Liu <liuyaping5427@outlook.com>
2022-07-11 18:12:56 +0800

    enum 和struct测试已完成

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -17,11 +17,13 @@ mod tests {
 
     #[test]
     fn ten_is_bigger_than_eight() {
+
         assert_eq!(10, bigger(10, 8));
     }
 
     #[test]
     fn fortytwo_is_bigger_than_thirtytwo() {
+
         assert_eq!(42, bigger(32, 42));
     }
 }
