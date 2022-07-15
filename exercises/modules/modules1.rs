820398c79e58a226812cdce77829860d53c97599
Yaping Liu <liuyaping5427@outlook.com>
2022-07-15 19:35:21 +0800

    modules测试已完成

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -9,7 +9,7 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
@@ -17,6 +17,6 @@ mod sausage_factory {
 
 fn main() {
     // use sausage_factory;
-    crate::sausage_factory::get_secret_recipe();
+    sausage_factory::make_sausage();
    //sausage_factory::get_secret_recipe().;
 }
